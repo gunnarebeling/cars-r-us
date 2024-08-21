8f3b40ef1b2b0b434daa4ecf6332e5f60c71127d
gunnar ebeling <gunnarebeling@Mac-mini.attlocal.net>
2024-08-21 15:59:13 -0500

    finished styling

--- scripts/main.js
+++ scripts/main.js
@@ -13,35 +13,35 @@ const render = async () =>{
     const orderHTML = await OrdersList()
     
     const html = `
-        <h1>CARS-R-US</h1>
-        <div id="main-container>
-            <div id="choices-container>
-                <div id="paint-choice">
+        <h1 id="title" class="text-center mt-3 mb-5">CARS-R-US</h1>
+        <div id="main-container class="container">
+            <div id="choices-container" class="row border justify-content-center mx-3 mb-3 pt-5 pb-3" style="gap:30px">
+                <div id="paint-choice" class="col-md-5 p-3 text-center border border-4 shadow choice-container">
                     <h2>Paint</h2>
                     <!--painthtml-->
                     ${paintHTML}
                 </div>
-                <div id="interior-choice">
+                <div id="interior-choice" class="col-md-5 p-3 text-center border border-4 shadow choice-container"  >
                     <h2>Interior</h2>
                     <!--interiorHTML-->
                     ${interiorHTML}
                 </div>
-                <div id="technology-choice">
+                <div id="technology-choice" class="col-md-5 p-3 text-center border border-4 shadow choice-container">
                     <h2>Technology</h2>
                     <!--technologiesHTML-->
                     ${technologiesHTML}
                 </div>
-                <div id="wheel-choice">
+                <div id="wheel-choice" class="col-md-5 p-3 text-center border border-4 shadow choice-container">
                     <h2>Wheels</h2>
                     <!--wheelsHTML-->
                     ${wheelsHTML}
                 </div>
-                <div>
+                <div class="d-flex justify-content-center">
                     ${submitbuttonHTML}
                 </div>
             </div>
-            <div id="orders">
-                <h2>Orders</h2>
+            <div id="orders" class="m-4 d-inline-block ">
+                <h2 class="ms-3">Orders</h2>
                 <div>
                     <!--ordersList-->
                     ${orderHTML}
